5ab4395890f7b55ee6f73ddd292359682f6357e4
yuan4101 <juan09ante@gmail.com>
2025-09-15 12:28:18 -0500

    about section fix for mobile

--- app/components/layout/server/footer.tsx
+++ app/components/layout/server/footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-[var(--color-bg)] p-6 mt-auto">
+    <footer className="bg-[var(--color-bg)] md:p-6 mt-auto">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center">
           <div className="font-bold">
-            <h3>Elisa&CO Hairclips</h3>
+            <Link href="/about">Elisa&CO Hairclips</Link>
           </div>
           <div className="text-lg px-2 hidden md:block">|</div>
           <div className="">
