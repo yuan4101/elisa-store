3d60c0e89aa6f3f40f8bdf523ad8a248eefef765
yuan4101 <juan09ante@gmail.com>
2025-10-28 16:22:49 -0500

    playwright and supabase

--- app/api/supabase-server.ts
+++ app/api/supabase-server.ts
@@ -1,12 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const errorMessage = "No se pudo conectar a la base de datos";
+if (!supabaseUrl) {
+  throw new Error(errorMessage, {cause: "NEXT_PUBLIC_SUPABASE_URL no esta definida"});
+} else if (!supabaseKey) {
+  throw new Error(errorMessage, {cause: "SUPABASE_SERVICE_ROLE_KEY no esta definida"});
+}
 
 export const supabaseServer = createClient(supabaseUrl, supabaseKey);
-
-// PRODUCTION
-export const TABLE = "products";
-
-// TEST
-//export const TABLE = "products_duplicate";
---
+++ temp-images/readme.txt
@@ -0,0 +1,3 @@
+lg -> 1700x1700 also Orginal size used 1772x1772)
+md -> 600x600
+sm -> 200x200
